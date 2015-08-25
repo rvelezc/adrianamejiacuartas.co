ebf9c4251b98ebef5ba33e3ffb7ff8a95d565f71
Rafael Velez <rafael.velez.c@gmail.com>
2015-08-25 11:02:24 -0500

    Update Config.php

--- public_html/assets/php/Config.php
+++ public_html/assets/php/Config.php
@@ -3,16 +3,16 @@
 //Email Settings
 define('MAIL_SERVER','smtp.gmail.com');             // Specify main and backup SMTP servers
 define('MAIL_USER','rafael.velez.c@gmail.com');     // SMTP username
-define('MAIL_PASSWORD','R4f43lV3l3z');                 // SMTP password
+define('MAIL_PASSWORD','');                 // SMTP password
 define('MAIL_SECURITY','tls');                      // Enable TLS encryption, `ssl` also accepted
 define('MAIL_PORT',587);                            // TCP port to connect to
 define('MAIL_FROM' , 'rafael.velez.c@gmail.com');   
 define('MAIL_FROM_NAME','Rafael Velez');
 
 //TWITTER
-define('TWITTER_OAUTH_ACCESS_TOKEN',"164422146-NYZDGcjKde0JtkmLgHVlOaSqlkI0WhPRaxutbH39");             
-define('TWITTER_OAUTH_ACCESS_TOKEN_SECRET','t3qf0UDTCrrvFMQFAyrG39NEaNwQZvKaHXxfYuFF3qqN8');
-define('TWITTER_CONSUMER_KEY','4TQNpWDLjDDdmV7KukfonHa7O');
-define('TWITTER_CONSUMER_SECRET','u1qAD52cW3lX9oGLrkzDg4JP4nZkDBClwk3fryCUVoh2qS1IO3');
+define('TWITTER_OAUTH_ACCESS_TOKEN',"");             
+define('TWITTER_OAUTH_ACCESS_TOKEN_SECRET','');
+define('TWITTER_CONSUMER_KEY','');
+define('TWITTER_CONSUMER_SECRET','');
 
 ?>
